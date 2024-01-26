633d77229d7fe9240947feaa4b03f4f08e4ef4de
faeze-Imantalab <146985472+faeze-Imantalab@users.noreply.github.com>
2024-01-26 17:00:18 +0330

    Added health bar
    Added health bar to the game

---
+++ FinalProject-part05/Assets/ConnectToServer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    private Loading loading;
+    void Start()
+    {
+        loading=Loading.instance;
+
+        if (!PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.ConnectUsingSettings();
+            loading.Set(true);
+        }
+        else
+        {
+            loading.Set(false);
+        }
+   
+
+        
+    }
+
+    public override void OnConnected()
+    {
+        base.OnConnected();
+        
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        loading.Set(false);
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+}
---
+++ FinalProject-part05/Assets/gameOver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class gameOver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject gameOverUi;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<FirstPersonController>().enabled = false;
+            gameOverUi.SetActive(true);
+        }
+           
+    }
+
+    public void ClickQuit()
+    {
+        Application.Quit();
+    }
+}
